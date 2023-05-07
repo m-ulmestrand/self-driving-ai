4c44972b8e2cb97d7b133e4c0d95b9d148c31258
Mattias Ulmestrand <m.ulmestrand@gmail.com>
2023-05-07 22:46:42 +0200

    Nicer NN

--- anim_nn.py
+++ anim_nn.py
@@ -6,7 +6,11 @@ class DrawNet(Scene):
     def construct(self):
         n_neurons = np.array([7, 10, 8, 4], dtype='intc')
 
-        neurons_x = np.linspace(-3, 3, len(n_neurons))
+        start_neuron_x = -3
+        end_neuron_x = 3
+        neurons_x = np.linspace(start_neuron_x, end_neuron_x, len(n_neurons))
+        neurons_x[0] = np.mean(neurons_x[[0, 1]])
+        neurons_x[-1] = np.mean(neurons_x[[-1, -2]])
         neurons_y = [None for _ in n_neurons]
         max_neurons = max(n_neurons)
         y_scale = 6
@@ -16,26 +20,61 @@ class DrawNet(Scene):
             neurons_y[i] -= neurons_y[i].mean()
             neurons_y[i] *= y_scale
         
-        circles = [[None for _ in range(n_neurons[i])] for i in range(len(n_neurons))]
+        neurons = np.array([[None for _ in range(n_neurons[i])] for i in range(len(n_neurons))], dtype=object)
 
         for i, (x, ys) in enumerate(zip(neurons_x, neurons_y)):
             for j, y in enumerate(ys):
                 coord = np.array([x, y, 0])
-                circles[i][j] = Circle(0.15, color=WHITE, fill_color=BLACK, fill_opacity=1, stroke_width=1.5).move_to(coord)
+                neurons[i][j] = Circle(0.15, color=WHITE, fill_color=BLACK, fill_opacity=1, stroke_width=1.5).move_to(coord)
         
-        self.play(*[FadeIn(circle) for circle in circles[0]], run_time=1)
+        self.play(*[FadeIn(neuron) for neuron in neurons[0]], run_time=1)
         self.wait(2)
 
         in_features = [r"d_{-72}", r"d_{-36}", r"d_{0}", r"d_{36}", r"d_{72}", r"v", r"\theta_{\text{wheels}}"]
         in_features = [MathTex(feat, font_size=30) for feat in in_features]
         x = neurons_x[0] - 1.5
         x_start = x + 0.4
+        in_arrows = [None] * len(neurons[0])
 
-        for txt, y in zip(in_features, neurons_y[0]):
+        for i, (txt, y) in enumerate(zip(in_features, neurons_y[0])):
             txt: MathTex
             txt.move_to([x, y, 0])
-            arrow = Arrow(start=[x_start, y, 0], end=[neurons_x[0], y, 0])
-            self.play(Write(txt), Create(arrow))
+            in_arrows[i] = Arrow(start=[x_start, y, 0], end=[neurons_x[0], y, 0])
+            self.play(Write(txt), Create(in_arrows[i]))
+
+        self.play(*[FadeIn(neuron) for neuron in neurons[-1]], run_time=1)
+        out_features = [r"+\Delta \theta", r"-\Delta \theta", r"+v", r"-v"]
+        out_features = [MathTex(feat, font_size=30) for feat in out_features]
+        x = neurons_x[-1] + 1.5
+        x_end = x - 0.2
+        out_arrows = [None] * len(neurons[-1])
+
+        for i, (txt, y) in enumerate(zip(out_features, neurons_y[-1])):
+            txt: MathTex
+            txt.move_to([x, y, 0])
+            out_arrows[i] = Arrow(start=[neurons_x[-1], y, 0], end=[x_end, y, 0])
+            self.play(Write(txt), Create(out_arrows[i]))
+        
+        lines_first = [None] * (n_neurons[0] * n_neurons[-1])
+
+        x1, x2 = neurons_x[[0, -1]]
+        j = 0
+        for y1 in neurons_y[0]:
+            for y2 in neurons_y[-1]:
+                lines_first[j] = Line([x1, y1, 0], [x2, y2, 0], color=GREY, stroke_width=1)
+                lines_first[j].set_z_index(neurons[0][0].z_index - 1)
+                j += 1
+
+        """line_anims = Succession(*[Create(line) for line in lines_first])
+        line_group = Group(*lines_first)
+        self.play(line_anims, run_time=3)"""
+        line_group = Group(*lines_first)
+        self.play(FadeIn(line_group), run_time=0.5)
+        self.wait(2)
+
+        shift_first = [start_neuron_x - neurons_x[0], 0, 0]
+        shift_last = [end_neuron_x - neurons_x[-1], 0, 0]
+        neurons_x = np.linspace(start_neuron_x, end_neuron_x, len(n_neurons))
 
         lines = [[None for _ in range(n_neurons[i] * n_neurons[i + 1])] for i in range(len(n_neurons) - 1)]
 
@@ -44,25 +83,22 @@ class DrawNet(Scene):
             for y1 in neurons_y[i]:
                 for y2 in neurons_y[i + 1]:
                     lines[i][j] = Line([x1, y1, 0], [x2, y2, 0], color=GREY, stroke_width=1)
-                    lines[i][j].set_z_index(circles[0][0].z_index - 1)
+                    lines[i][j].set_z_index(neurons[0][0].z_index - 1)
                     j += 1
 
-        for circle_collection, line_collection in zip(circles[1:], lines):
-            self.play(
-                *[FadeIn(line) for line in line_collection],
-                *[FadeIn(circle) for circle in circle_collection],
-                run_time=0.5
-            )
+        flattened_lines = Group(*[item for sublist in lines for item in sublist])
+        in_neurons = neurons[0]
+        out_neurons = neurons[-1]
+        neurons = neurons[1:-1]
+        self.play(
+            *[neur.animate.shift(shift_first) for neur in in_neurons],
+            *[feat.animate.shift(shift_first) for feat in in_features],
+            *[arr.animate.shift(shift_first) for arr in in_arrows],
+            *[neur.animate.shift(shift_last) for neur in out_neurons],
+            *[feat.animate.shift(shift_last) for feat in out_features],
+            *[arr.animate.shift(shift_last) for arr in out_arrows],
+            Transform(line_group, flattened_lines),
+            *[FadeIn(neuron) for neuron_collection in neurons for neuron in neuron_collection]
+        )
 
-        out_features = [r"+\Delta \theta", r"-\Delta \theta", r"+v", r"-v"]
-        out_features = [MathTex(feat, font_size=30) for feat in out_features]
-        x = neurons_x[-1] + 1.5
-        x_end = x - 0.2
-
-        for txt, y in zip(out_features, neurons_y[-1]):
-            txt: MathTex
-            txt.move_to([x, y, 0])
-            arrow = Arrow(start=[neurons_x[-1], y, 0], end=[x_end, y, 0])
-            self.play(Write(txt), Create(arrow))
-        
         self.wait(2)
