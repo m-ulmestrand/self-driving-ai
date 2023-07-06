897b3ab35640d11e388c85488e8ab826064443f8
Mattias Ulmestrand <m.ulmestrand@gmail.com>
2023-07-06 20:18:45 +0200

    Prettier train.py and better visualization

--- train.py
+++ train.py
@@ -16,20 +16,32 @@ import sys
 
 
 def main():
-    box_size = 100
     runs = 1500
-    epsilon_scale = 3000
     training_track_numbers = [1, 8, 15]
     n_epochs = 1
-    race_car = RacingAgent(box_size=box_size, epsilon_scale=epsilon_scale, buffer_behaviour="discard_old", turning_speed=0.125,
-                        epsilon_start=0.5, epsilon_final=0., r_min=5., buffer_size=5000, seq_length=1, network_type=DenseNetwork,
-                        hidden_neurons=(32,32,32), target_sync=30, generation_length=1000, track_numbers=training_track_numbers,
-                        turn_radius_decay=1., append_scale=20)
+    race_car = RacingAgent(
+        box_size=100, 
+        epsilon_scale=3000, 
+        buffer_behaviour="discard_old", 
+        turning_speed=0.125,
+        epsilon_start=0.5, 
+        epsilon_final=0., 
+        r_min=5., 
+        buffer_size=5000, 
+        seq_length=1, 
+        network_type=DenseNetwork,
+        network_params=(32,32,32), 
+        target_sync=100, 
+        generation_length=1000, 
+        track_numbers=training_track_numbers,
+        turn_radius_decay=1., 
+        append_scale=20,
+        name='agent_dense2'
+    )
 
     # Change this to initialize and train a new agent.
     # Trained agents are saved at ./build, load just the name without the .pt extension.
     # Both the final agent and the best performing one are saved.
-    race_car.save_name = 'agent_attn'
     race_car.load_network(name=race_car.save_name)
     race_car.store_track(training_track_numbers[0])
 
@@ -43,8 +55,11 @@ def main():
     x_data = np.arange(runs)
     dists = np.zeros(runs)
     avg_dists = np.zeros(runs)
-    plot, = ax.plot(dists, color="black")
-    avg_plot, = ax.plot(dists, color="red", linestyle="--")
+    best_dists = np.zeros(runs)
+    plot, = ax.plot(dists, color="black", label="Distance")
+    avg_plot, = ax.plot(avg_dists, color="red", linestyle="dashed", label="Running average")
+    best_plot, = ax.plot(best_dists, color="dodgerblue", label="Maximum")
+    ax.legend(loc="upper left")
     fig.canvas.draw()
     plt.show(block=False)
 
@@ -66,20 +81,30 @@ def main():
                     f"Epsilon: {race_car.get_epsilon():.2f}, " \
                     f"Loss: {race_car.total_loss:.4f}"
         sys.stdout.write(message)
-        dists[i] = race_car.distance
+
+        current_dist = race_car.distance
+        if current_dist > y_max:
+            y_max = current_dist
+            ax.set_ylim([0, y_max * 1.1])
+
+        if current_dist > best_dists[i - 1]:
+            best_dists[i] = current_dist
+        else:
+            best_dists[i] = best_dists[i - 1]
+
+        dists[i] = current_dist
         avg_dists[i] = dists[max(0, i - avg_interval): i + 1].mean()
         race_car.reinitialise()
 
-        if dists[i] > y_max:
-            y_max = dists[i]
-            ax.set_ylim([0, y_max * 1.1])
-
         plot.set_data(x_data[:i + 1], dists[:i + 1])
         avg_plot.set_data(x_data[:i + 1], avg_dists[:i + 1])
+        best_plot.set_data(x_data[:i + 1], best_dists[:i + 1])
+        ax.draw_artist(best_plot)
         ax.draw_artist(plot)
         ax.draw_artist(avg_plot)
         fig.canvas.draw()
         fig.canvas.flush_events()
+        fig.savefig("./figures/history.png", format="png")
 
     race_car.save_network('final_' + race_car.save_name)
 
